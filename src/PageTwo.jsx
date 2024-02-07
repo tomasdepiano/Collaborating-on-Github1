9359c42da751509b2f7de2680cbdb72f8e0dee1a
Mounika Krishna <mounikaa.krishna@gmail.com>
2024-02-06 21:13:23 -0700

    worked on page 2 user profile

--- src/PageTwo.jsx
+++ src/PageTwo.jsx
@@ -1,9 +1,18 @@
+import "./PageTwo.css";
 export default function PageTwo() {
-
-
   return (
     <>
-    Page Two
+      <div className="main-container">
+        <div className="user-pic"></div>
+        <h3>Jessica Randall</h3>
+        <h5>London,United Kingdom</h5>
+        <h6> Front-end developer and avid reader.</h6>
+        <button>GitHub</button>
+        <button>Frontend Mentor</button>
+        <button>LinkedIn</button>
+        <button>Twitter</button>
+        <button>Instagram</button>
+      </div>
     </>
   );
 }
